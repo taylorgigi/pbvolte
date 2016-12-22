3f579ae47b494b0b84a098e450ca224196098a63
taylorgigi <ysmgigi@163.com>
2016-12-22 10:00:11 +0800

    fix bug of no instance of transac_queue

--- transaction_thread.c
+++ transaction_thread.c
@@ -3,11 +3,13 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+list_t *transac_queue = NULL;
+
 void *transaction_thread(void *arg) {
 	// to-do: set cpu affinity
 	pthread_detach(pthread_self());
 	int idx = (int)(unsigned long)arg;
-	list_t *que = transac_queue[idx];
+	list_t *que = &transac_queue[idx];
 
 	pthread_exit(NULL);
 	return NULL;
