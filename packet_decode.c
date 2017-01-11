6daaaebd1418e6a2fa15fe79017833c8ec5c5439
taylorgigi <ysmgigi@163.com>
2017-01-11 10:00:52 +0800

    fix bug of printf format

--- packet_decode.c
+++ packet_decode.c
@@ -1,10 +1,11 @@
 #include "packet_decode.h"
+#include <stdio.h>
 
 int packet_decode(packet_t *pkt, uint8_t *payload, uint16_t len) {
 	int code;
 	code = decode_ethernet(pkt, payload, len);
 	if(code < 0) { // decode failed
-		printf("decode_ethernet error, %s, %s\n", __FILE__, __LINE__);
+		printf("decode_ethernet error, %s, %d\n", __FILE__, __LINE__);
 		return -1;
 	}
 }
